ffbaa429755e551a49520c3fbfd20448975f1488
Chloe <chloe.stinetorf@gmail.com>
2013-04-17 12:05:17 -0400

    moved old portfolio folder to another directory

--- chloe portfolio firstdraft copy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-R20130215ChloePortfolio::Application.config.secret_token = '42a493316bf997bc922091d2143e46079c8aa1f2709530607897f13bb34196ab9251ec17f2a1407d97c65917ce4eae05c9b7ebf1c83856f715f54528532caab5'
--- chloe portfolio firstdraft copy/config/routes.rb
+++
@@ -1,5 +0,0 @@
-R20130215ChloePortfolio::Application.routes.draw do
-
-  root :to => 'projects#index'
-  get '/' => 'projects#index'
-end
